0ea1ef652b45219a3530c8af223294baa6c01160
dhruvnagpal95 <dhruv.nagpal@kellogg.northwestern.edu>
2023-01-31 20:01:01 +0000

    adding hand generator

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -20,3 +20,35 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+cards = Array.new
+index1=0
+index3=0
+
+loop do   
+    if index1 == ranks.length
+        break
+    end
+    index2 = 0
+    loop do
+        if index2 == suits.length
+            break
+        end
+        cards[index3] = "#{ranks[index1]}"+" of " + "#{suits[index2]}"
+        index2 = index2+1
+    index3 = index3+1
+    end
+    index1 = index1+1
+end
+
+# puts cards
+
+index4=0
+
+loop do
+    if index4 == 5
+        break
+    end
+    puts cards[rand(51)]
+index4=index4+1
+end
